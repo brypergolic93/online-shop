0f43555eb3e942f92637d814b5f5c572fafd69bd
Bryan Little <bryanlittle613@gmail.com>
2022-06-08 07:20:00 +0200

    Re-organize customer views, add login, signup, and includes views

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const path = require('path');
 
 const app = express();
 
+const authRoutes = require("./routes/auth.routes");
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(authRoutes);
+
 
 db.connectToDatabase().then(function () {
     app.listen(3000);
