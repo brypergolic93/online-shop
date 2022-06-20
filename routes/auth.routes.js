77fff7c559b96a1652fd378a4dc05ecc7a3047fe
Bryan Little <bryanlittle613@gmail.com>
2022-06-20 07:25:06 +0200

    Complete auth form styles

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.get("/signup", authController.getSignup);
+router.post("/signup", authcontroller.signup);
 router.get("/login", authController.getLogin);
 
 module.exports = router;
